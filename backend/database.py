338ac7a6376c9ce3a069503677940d7d3a55d771
malikawannasi <oiannaci.mlykh@gmail.com>
2024-11-12 17:07:10 +0100

    fix job

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 
 # Define the database URL for SQLite
 SQLALCHEMY_DATABASE_URL = "sqlite:///./titanic.db"
